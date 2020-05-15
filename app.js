383e48fb9b479dd0c35b7aaa7b566d6b6cf04e61
jbj616 <jbj616@gmail.com>
2020-05-15 16:18:31 +0900

    feat : 교수님 자료 반영

--- app.js
+++ app.js
@@ -22,60 +22,70 @@ app.set("views", __dirname + "/views");
 app.use("/", static(path.join(__dirname, "views/page")));
 
 app.get("/", (req, res) => {
-  res.render("page/HomePage");
+	res.render("page/HomePage");
 });
 
 //members tab
 app.get("/alumni", (req, res) => {
-  res.render("page/Member/AlumniPage");
+	res.render("page/Member/AlumniPage");
 });
 
 app.get("/members", (req, res) => {
-  res.render("page/Member/MembersPage");
+	res.render("page/Member/MembersPage");
 });
 
 app.get("/professor", (req, res) => {
-  res.render("page/Member/ProfessorPage");
+	res.render("page/Member/ProfessorPage");
 });
 
 //Research Page
 app.get("/field", (req, res) => {
-  res.render("page/Research/FieldPage");
+	res.render("page/Research/FieldPage");
 });
 
 app.get("/method", (req, res) => {
-  res.render("page/Research/MethodPage");
+	res.render("page/Research/MethodPage");
 });
 
 app.get("/project", (req, res) => {
-  res.render("page/Research/ProjectPage");
+	res.render("page/Research/ProjectPage");
+});
+
+//Introduce Page
+app.get("/introduce", (req, res) => {
+	res.render("page/IntroducePage");
 });
 
 //Career Tab
 app.get("/career", (req, res) => {
-  res.render("page/CareerPage");
+	res.render("page/CareerPage");
 });
 
 //NewsPage
 app.get("/newspaper", (req, res) => {
-  res.render("page/NewsPage");
+	res.render("page/NewsPage");
 });
 
 //patents
 app.get("/patent", (req, res) => {
-  res.render("page/PatentsPage");
+	res.render("page/PatentsPage");
 });
 
 //publication
 app.get("/publication", (req, res) => {
-  res.render("page/PublicationPage");
+	res.render("page/PublicationPage");
+});
+
+//Lecture
+app.get("/lecture", (req, res) => {
+	res.render("page/LecturePage");
 });
 
 //에러 처리
 var errorHandler = expressErrorHandler({
-  static: {
-    "404": "./front/erroor.html",
-  },
+	static: {
+		"404": "./front/erroor.html",
+	},
 });
 
 app.use(expressErrorHandler.httpError(404));
@@ -83,5 +93,5 @@ app.use(errorHandler);
 
 //서버 생성
 http.createServer(app).listen(app.get("port"), () => {
-  console.log("익스프레스로 웹 서버를 실행함: " + app.get("port"));
+	console.log("익스프레스로 웹 서버를 실행함: " + app.get("port"));
 });
